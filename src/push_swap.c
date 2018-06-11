b344a90b713e45e4df1528ba1564170a818cb02b
Alex Tiniakov <39567406+AlexTiniakov@users.noreply.github.com>
2018-06-11 23:17:16 +0300

    Update push_swap.c1

--- src/push_swap.c
+++ src/push_swap.c
@@ -16,7 +16,10 @@ int		main(int ac, char **av)
 {
 	t_stack		stack;
 
+   stack.a = NULL;
+   stack.b = NULL;
 	stack.size_a = ac - 1;
+   stack.size_b = 0;
 	if (!ft_create_stack(ac, av, &stack, 0))
 		return (write(1, "Error\n", 6));
 	ft_sort_hard(&stack);
