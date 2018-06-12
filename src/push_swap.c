fda52197342ffcd315ab0c060be1a89abb8aef28
Alex Tiniakov <39567406+AlexTiniakov@users.noreply.github.com>
2018-06-12 12:55:19 +0300

    Update push_swap.c

--- src/push_swap.c
+++ src/push_swap.c
@@ -24,10 +24,17 @@ void ft_check_rez(t_stack *stack)
 	//rb\nra\n -> rr\n
 	//rra\nrrb\n -> rrr\n
 	//rrb\nrra\n -> rrr\n
+
+    char *str;
+
     while (ft_strstr(stack->rez, “pa\npb\n”) || ft_strstr(stack->rez, “pb\npa\n”)\
 || ft_strstr(stack->rez, “ra\nrra\n”) || ft_strstr(stack->rez, “rra\nra\n”) ||\
 ft_strstr(stack->rez, “rb\nrrb\n”) || ft_strstr(stack->rez, “rrb\nrb\n”))
-	    ;//del using memmove or strjoin
+	    //del using memmove or strjoin
+    if ((str = ft_strstr(stack->rez, “pa\npb\n”))
+        tab = stack->rez;
+        tmp = ft_strncpy();
+        stack->rez = ft_strjoin()
     while (ft_strstr(stack->rez, “ra\nrb\n”) || ft_strstr(stack->rez, “rb\nra\n”)\
 || ft_strstr(stack->rez, “rra\nrrb\n”) || ft_strstr(stack->rez, “rrb\nrra\n”))
        ;//change to r 
